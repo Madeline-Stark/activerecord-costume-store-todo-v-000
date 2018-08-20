4934d81a6881cb51a04e7b5be735d1dfb3cd5d2f
Madeline Stark <madeline.stark1@gmail.com>
2018-08-20 16:17:12 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,9 +2,13 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
   def change
     create_table :haunted_houses do |t|
       t.string :name
+      t.string :location
+      t.string :theme
       t.float :price
-      t.string :size
-      t.string :image_url
+      t.boolean :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.string :description 
 
       t.timestamps
     end
