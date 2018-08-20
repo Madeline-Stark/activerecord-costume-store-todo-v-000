a906b0616e147fefb70ca1cc9a4f656b53052e17
Madeline Stark <madeline.stark1@gmail.com>
2018-08-20 16:13:36 +0000

    automatically backed up by learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,13 +1,2 @@
-
 class Costume < ActiveRecord::Base
-  def change
-    create_table :costumes do |t|
-      t.string :name
-      t.float :price
-      t.string :size
-      t.string :image_url
-      
-      t.timestamps
-    end
-  end
 end
